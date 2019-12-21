204e3440dc4fc56d4cce2b08c9a31dda1f469855
Ratbek Nurlanbek uulu <ratbeknurlanov@gmail.com>
2019-12-21 16:10:53 +0500

    Создавать опрос

--- app.js
+++ app.js
@@ -5,6 +5,38 @@ const app = express();
 
 const polls = new Map();
 
+app.route('/api/poll')
+    .post((req, res, err) => {
+        const newPollId = uuid.v4();
+
+        polls.set(newPollId, {
+            id: newPollId,
+            questions: [
+                {
+                    id: uuid.v4(),
+                    question: 'Hello',
+                    guesses: ['one', 'two', 'three', 'four']
+                }
+            ]
+        });
+
+        res.redirect(`/poll/${newPollId}`);
+    });
+
+app.route('/api/poll/:id')
+    .get((req, res, err) => {
+        const pollId = req.params.id;
+
+        if (polls.has(pollId)) {
+            res.send(polls.get(pollId))
+                .end();
+        }
+
+        res.status(400)
+            .end();
+    });
+
+
 app.listen(5000, () => {
     console.log('Listening on 5000');
 });
