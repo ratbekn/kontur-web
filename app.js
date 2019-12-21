d632db2b5d42c956d84f602d98c25db2c24771cd
a.savichev <a.savichev@skbkontur.ru>
2019-12-21 14:14:16 +0500

    oop please

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res, err) => {
-    res.send('Hello, World!');
+    const hello_world_provider = new HelloWorldProvider();
+    const msg = hello_world_provider.get();
+    res.send(msg);
 });
 
 app.listen(5000, () => {
