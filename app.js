c528f220143303998fc5a02a97a2a66ce5e3b571
Ratbek Nurlanbek uulu <ratbeknurlanov@gmail.com>
2019-12-21 15:14:04 +0500

    Подключить `uuid`, создать словарь опросов

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const uuid = require('uuid');
 
 const app = express();
 
 app.get('/', (req, res, err) => {
     res.send('Hello, World!');
 });
+
+const polls = new Map();
 
 app.listen(5000, () => {
     console.log('Listening on 5000');
